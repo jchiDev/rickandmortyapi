fc40a8257d9bac6c605c04bf4240e5e3c76daeff
Juan Carlos <jchi.dev@gmail.com>
2021-11-15 11:53:25 -0600

    Updates

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import { itemReducer, selecteditemReducer } from './itemReducer';
-
-const reducers = combineReducers({
-    allItems: itemReducer,
-    item: selecteditemReducer,
-});
-
-export default reducers
